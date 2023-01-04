b73b771ad7746932ac4723bcc51ce4d1880d2818
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-04 16:54:20 +0700

    :alien: Add Auth Page

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,9 +1,11 @@
-import React, { Suspense, lazy } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const HomePage = lazy(() => import("../pages/HomePage"));
 const ProductPage = lazy(() => import("../pages/ProductPage"));
 const AboutPage = lazy(() => import("../pages/AboutPage"));
+const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
+const RegisterPage = lazy(() => import("../pages/auth/RegisterPage"));
 
 export default function AppRouter() {
   return (
@@ -13,6 +15,8 @@ export default function AppRouter() {
           <Route path="/" element={<HomePage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/register" element={<RegisterPage />} />
         </Routes>
       </Suspense>
     </Router>
